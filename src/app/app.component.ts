8e62112f87f64beaaeeba34e8cab90349cc83bd3
Arun6381 <kumararun28092@gmail.com>
2025-03-13 13:25:23 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,Menu, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyApp';
+
+  constructor(private primeng: PrimeNG) {}
+
+
+
+
+
+
+
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.primeng.ripple.set(true);
+
+        this.primeng.zIndex = {
+            modal: 1100,    // dialog, sidebar
+            overlay: 1000,  // dropdown, overlaypanel
+            menu: 1000,     // overlay menus
+            tooltip: 1100   // tooltip
+        };
+        this.items = [
+            {
+                label: 'Options',
+                items: [
+                    {
+                        label: 'Refresh',
+                        icon: 'pi pi-refresh'
+                    },
+                    {
+                        label: 'Export',
+                        icon: 'pi pi-upload'
+                    }
+                ]
+            }
+        ];
+        console.log(this.items)
+    }
 }
